20052c774c35241f99bb8036b90c2a1d1657ee5c
Rafał <brotlon.corp@gmail.com>
2023-05-26 15:58:57 +0200

    added ScrollSpy

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,21 @@ const navbar = document.querySelector('.navbar')
 const burgerBtn = navbar.querySelector('.navbar__burger-btn')
 const navLinksBox = navbar.querySelector('.navbar__links-box')
 
+const navLinks = document.querySelectorAll('.navbar__nav-link')
+const scrollSpySections = document.querySelectorAll('.section')
+const lastSection = document.querySelector('.navbar__nav-item:last-child a')
+const mobileNav = document.querySelector('.navbar__burger-btn-box')
+const desktopNav = document.querySelector('.navbar__links-box')
+
 const showMorePicturesBtn = document.querySelector('.gallery__show-all-btn')
 const hiddenPictures = document.querySelectorAll('.gallery__picture')
 const footerYear = document.querySelector('.footer__year')
 
-
+let root = document.documentElement;
+let navHeight
 
 let mobileNavTransitionTime = 351
+
 
 
 const hideInactiveArea = () => {
@@ -36,6 +44,36 @@ const hideMobileNav = e => {
 }
 
 
+const handleScrollSpy = () => {
+    const sections = []
+
+    if (document.body.offsetWidth < 992) {
+        navHeight = mobileNav.offsetHeight
+    } else {
+        navHeight = desktopNav.offsetHeight
+    }
+
+    scrollSpySections.forEach(section => {
+        if (window.scrollY <= section.offsetTop + section.offsetHeight - navHeight - 2) {
+            sections.push(section)
+
+            const activeSection = document.querySelector(`.navbar__nav-link[href*="${sections[0].id}"]`)
+
+            navLinks.forEach(link => link.classList.remove('active'))
+
+            activeSection.classList.add('active')
+        }
+    })
+
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 20) {
+        navLinks.forEach(link => link.classList.remove('active'))
+
+        lastSection.classList.add('active')
+    }
+
+}
+
+
 const showHiddenPictures = () => {
     hiddenPictures.forEach(item => {
         item.classList.add('show')
@@ -54,6 +92,7 @@ const handleCurrentYear = () => {
 handleCurrentYear()
 burgerBtn.addEventListener('click', handleMobileNav)
 window.addEventListener('click', hideMobileNav)
+window.addEventListener('scroll', handleScrollSpy)
 showMorePicturesBtn.addEventListener('click', showHiddenPictures)
 
 
